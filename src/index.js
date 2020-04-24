2c74ed41760340b8c5a7adf1ce8c0ee838db79a5
besoKing <alen.besenic@gmail.com>
2020-04-24 13:42:39 +0200

    commit on master

--- src/index.js
+++ src/index.js
@@ -9,4 +9,6 @@ const port = 3000
 app.get('/events', (req, res) => {
     res.json(data.event)
 })
+app.get('/category', (req, res)=>res.json(data.Category))
+app.get('category/:category/outdoor', (req, res)=>res.json(data.Category.Outdoor))
 app.listen(port, () => console.log(`Slušam na portu ${port}!`))
