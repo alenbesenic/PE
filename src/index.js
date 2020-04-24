30f4684f86e6e874a564242b8478090ddd10f165
besoKing <alen.besenic@gmail.com>
2020-04-24 11:08:47 +0200

    commit on master

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log("Hello world")
+import express from 'express';
+
+
+const app = express() 
+const port = 3000 
+
+
+app.get('/events', (req, res) => {
+    
+})
+app.listen(port, () => console.log(`Slušam na portu ${port}!`))
---
+++ src/memory.js
@@ -0,0 +1,11 @@
+let data ={
+    event:[
+        {
+            Location : 'Valovine beach (with ski lift), Stoja 37, 52100, Pula, Croatia',
+            Date: '01. svi 10:00 – 13:00',
+            Description:'Sea kayaking in the "wild side" of Pula, visit of the cliffs, climbing on the rocks and jumping off (optional), short break at the beach and time for snorkelling, swimming, relaxing. If possible, visit of the cave in kayaks, illuminated with head lights. Group size: 8 people!',
+            Event_Name:'Cliffs & Cave Kayaking (Pula)',
+            Event_Price: 380
+        }
+    ]
+}
