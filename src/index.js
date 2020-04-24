051f9a8674531969ef9b4fcfa614aa27440d5c0d
bmacan <58048622+bmacan@users.noreply.github.com>
2020-04-24 10:48:37 +0200

    commit on master

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log("Hello world")
+import express from 'express';
+
+
+const app = express() // instanciranje aplikacije
+const port = 3000 // port na kojem će web server slušati
+
+
+
+app.get('/', (req, res) => res.send('Hello World, ovaj puta preko browsera!'))
+app.get('/student', (req, res) => res.send('Ruta za studente...'))
+app.get('/primjer/student', (req, res) => res.send('Ugnježdena ruta'))
+
+
+
+
+app.listen(port, () => console.log(`Slušam na portu ${port}!`))
