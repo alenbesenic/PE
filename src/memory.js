7353669344a7c3b606e17d28a17dd0e11934f538
besoKing <alen.besenic@gmail.com>
2020-04-24 13:32:04 +0200

    commit on master

--- src/memory.js
+++ src/memory.js
@@ -27,8 +27,41 @@ let data ={
             Event_URL: 'https://www.facebook.com/events/830668674085593',
             Category:'NightLife'
         },
+    ],
+    Category:{
+        Outdoor:[
+            {
+                Location : 'Valovine beach (with ski lift), Stoja 37, 52100, Pula, Croatia',
+                Date: '01. svi 10:00 – 13:00',
+                Description:'Sea kayaking in the "wild side" of Pula, visit of the cliffs, climbing on the rocks and jumping off (optional), short break at the beach and time for snorkelling, swimming, relaxing. If possible, visit of the cave in kayaks, illuminated with head lights. Group size: 8 people!',
+                Event_Name:'Cliffs & Cave Kayaking (Pula)',
+                Event_Price: 380,
+                Event_URL: 'https://www.pulaoutdoor.com/events/cliffs-cave-kayaking-pula',
+            }
+        ],
+        NightLife:[
+            {
+                Location : 'Discotheque Magnus Pazin',
+                Date: '30. listopada 2020.',
+                Description:'Vojko V dolazi u Pazin, naravno, u Magnus! Uz nastup na Sea Star festivalu, izvođaču će ovo biti jedini nastup u Istri do jeseni 2020. -> nepropustivo!',
+                Event_Name:'VOJKO V ⎮ Magnus ⎮ Odgođeno za jesen',
+                Event_Price: 0 ,
+                Event_URL: 'https://www.facebook.com/events/2474358526138005',
+                Category:'NightLife'
+            },
+            {
+                Location : 'Klub Kotač',
+                Date: '25. Travnja 2020.',
+                Description:'Klub Kotač/Kotač TV DC Rojc/Pula subota 25.4.2020. 22:00h - livestream Kotač Facebook/Twitch',
+                Event_Name:'Corona Room #18: Fikcio Monger - live',
+                Event_Price: 0 ,
+                Event_URL: 'https://www.facebook.com/events/830668674085593',
+                Category:'NightLife'
+            }
+        ]
+    }
 
-    ]
+    
 }
 
 export default data
