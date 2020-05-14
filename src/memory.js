8b62091311b562e8bc00b0851549a900a14c4362
besoKing <alen.besenic@gmail.com>
2020-05-14 13:04:14 +0200

    commit on master

--- src/memory.js
+++ src/memory.js
@@ -36,6 +36,18 @@ let data ={
             Status:'Ongoing',
             PictureURL:'https://scontent.fzag1-1.fna.fbcdn.net/v/t1.0-9/94030770_1483323375174653_3687779909574328320_n.jpg?_nc_cat=102&_nc_sid=b386c4&_nc_ohc=fovWYeC2uesAX_5hPWx&_nc_ht=scontent.fzag1-1.fna&oh=7bedbc7fd72abbf1736a2e046ab6c676&oe=5EE308ED'
         },
+        {
+            id:10004,
+            Location: 'Gradska knjižnica i čitaonica Pula',
+            Date: '05.05.2020',
+            Description: 'I ove godine Knjižnica se pridružuje obilježavanju Dana grada Pule. Pozivamo vas da i ovogodišnji 5. svibnja, dan u kojem slavimo svoj grad, provedete uz knjige, izložbe i promišljanja o Puli.',
+            Event_Name: 'Dan grada Pule: Gladijator u tramvaju i virtualna šetnja starom Pulom',
+            Event_Price: 0,
+            Event_URL: 'https://gkc-pula.hr/hr/dogadanja/?post_id=2225&post_slug=dan-grada-pule-gladijator-u-tramvaju-i-vizz',
+            Category: 'Library',
+            Status: 'Done',
+            PictureURL: 'https://gkc-pula.hr/site_media/media/uploads/images/news/slikovnica_02.jpg.1024x0_q85.jpg.390x390_q92.jpg'
+        }
     ],
     Category:{
         Outdoor:[
