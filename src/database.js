ff298d158cdc75c77887d7e1a8b1dedb45cd3d5e
besoKing <alen.besenic@gmail.com>
2020-05-26 12:13:54 +0200

    mongodb

---
+++ src/database.js
@@ -0,0 +1,29 @@
+const mongo = require("mongodb")
+
+let connection_string = "mongodb+srv://admin:admin@cluster0-sep1a.mongodb.net/test?retryWrites=true&w=majority"
+
+let client = new mongo.MongoClient(connection_string, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+let db = null
+
+export default () => {
+    return new Promise((resolve, reject) => {
+
+        if (db && client.isConnected()) {
+            resolve(db)
+        } else {
+            client.connect(err => {
+                if (err) {
+                    reject("Došlo je do greške prilikom spajanja", err)
+                } else {
+                    console.log("Uspješno spajanje")
+                    db = client.db("PulaEvents")
+                    resolve(db)
+                }
+            })
+        }
+    })
+}
--- src/index.js
+++ src/index.js
@@ -2,34 +2,38 @@ const express = require('express');
 const data = require('./memory')
 const cors = require('cors')
 const path = require('path')
+import connect from './database'
 
 
-const app = express() 
+const app = express()
 
-
-
-const port = process.env.PORT || 3000 
+const port = process.env.PORT || 3000
 
 app.use(cors())
 app.use(express.json())
 
 //svi eventi
-app.get('/events', (req, res) => {
-    res.json(data.event)
+app.get('/events', async(req, res) => {
+    let db = await connect();
+
+    let cursor = await db.collection("Events").find()
+    let results = await cursor.toArray()
+
+    res.json(results)
 })
 
 
 //kategorije
-app.get('/category', (req, res)=>res.json(data.Category))
-app.get('/category/outdoor', (req, res)=>res.json(data.Category.Outdoor))
-app.get('/category/nightlife', (req, res)=>res.json(data.Category.NightLife))
-app.get('/category/library', (req, res)=>res.json(data.Category.Library))
+app.get('/category', (req, res) => res.json(data.Category))
+app.get('/category/outdoor', (req, res) => res.json(data.Category.Outdoor))
+app.get('/category/nightlife', (req, res) => res.json(data.Category.NightLife))
+app.get('/category/library', (req, res) => res.json(data.Category.Library))
 
 //otkazani eventi
-app.get('/canceledEvents', (req,res)=>{
+app.get('/canceledEvents', (req, res) => {
     let canceled = []
-    for(var item of data.event){
-        if(item.Status === 'Canceled'){
+    for (var item of data.event) {
+        if (item.Status === 'Canceled') {
             canceled.push(item)
         }
     }
@@ -37,13 +41,13 @@ app.get('/canceledEvents', (req,res)=>{
 })
 
 //nadolazeći eventi po datumima
-app.get('/upcomingEvents', (req, res)=>{
+app.get('/upcomingEvents', (req, res) => {
     let podaci = data.event
-    podaci.sort((a, b)=>{
-        if(a.Date>b.Date){
+    podaci.sort((a, b) => {
+        if (a.Date > b.Date) {
             return 1;
         }
-        if(a.Date<b.Date){
+        if (a.Date < b.Date) {
             return -1;
         }
         return 0;
@@ -52,11 +56,11 @@ app.get('/upcomingEvents', (req, res)=>{
 })
 
 //za dohvat jednog eventa
-app.get('/events/:id', (req, res)=>{
+app.get('/events/:id', (req, res) => {
     let id = req.params.id
     let doc = data.event
-    for(var item of doc){
-        if(id==item.id){
+    for (var item of doc) {
+        if (id == item.id) {
             res.json(item)
         }
     }
@@ -64,14 +68,14 @@ app.get('/events/:id', (req, res)=>{
 
 
 
-  if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
     // Static folder
     app.use(express.static(path.join(__dirname, "../public")))
-  
+
     // Handle SPA
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, '../public', 'index.html'))
     });
-  }
+}
 
 app.listen(process.env.PORT || port, () => console.log(`Slušam na portu ${port}!`))
